36bd1f9ea7f1243b4d393442df6bafd104a88e90
PawanK06 <pawan06kumar2003@gmail.com>
2024-08-08 15:55:19 +0530

    add production setup

--- frontend/src/components/appbar/navbar.tsx
+++ frontend/src/components/appbar/navbar.tsx
@@ -2,40 +2,41 @@ import React from 'react'
 import Logo from '../icons/Logo'
 import Link from 'next/link'
 import { Button } from '../ui/button'
-import {FaGithub, FaWallet} from 'react-icons/fa'
+import {FaWallet} from 'react-icons/fa'
+import { ModeToggle } from '../ModeToggle'
 
 const Navbar = () => {
     return (
-        <header className="py-4 border-b md:border-none fixed top-0 left-0 right-0 z-10 bg-white md:bg-white/0">
+        <header className="py-4 border-b md:border-none fixed top-0 left-0 right-0 z-10">
             <div className="container mx-auto px-4 ">
-                <div className="flex justify-between items-center md:border md:p-2.5 rounded-xl max-w-2xl lg:max-w-4xl mx-auto md:bg-white/90 md:backdrop:blur-sm">
+                <div className="flex justify-between items-center md:border md:p-2.5 rounded-xl max-w-2xl lg:max-w-4xl mx-auto backdrop-blur-0blur-sm">
                     <div>
-                        <div className="border h-10 w-10 rounded-lg inline-flex justify-center items-center shadow-inner">
-                            <Logo className="h-8 w-8" />
+                        <div className="border h-10 w-10 rounded-lg inline-flex justify-center items-center shadow-inner bg-black shadow-white/70">
+                            <Logo className="h-8 w-8 "/>
                         </div>
                     </div>
-                        <div className="hidden md:block">
+                        <div className="hidden md:block ">
                             <nav className="flex gap-8 text-sm">
                                 <Link
-                                    className="text-black/70 hover:text-black transition"
+                                    className="hover:underline"
                                     href="#"
                                 >
                                     Products
                                 </Link>
                                 <Link
-                                    className="text-black/70 hover:text-black transition"
+                                    className="hover:underline"
                                     href="#"
                                 >
                                     FAQ
                                 </Link>
                                 <Link
-                                    className="text-black/70 hover:text-black transition"
+                                    className="hover:underline"
                                     href="#"
                                 >
                                     Company
                                 </Link>
                                 <Link
-                                    className="hidden lg:block text-black/70 hover:text-black transition"
+                                    className="hover:underline"
                                     href="#"
                                 >
                                     Blogs
@@ -43,7 +44,8 @@ const Navbar = () => {
                             </nav>
                         </div>
                         <div className="flex gap-4 items-center">
-                            <Button className='flex gap-2 shadow-inner shadow-white/70'>
+                            <ModeToggle/>
+                            <Button className='flex gap-2 shadow-inner shadow-white/70 dark:shadow-black/70'>
                             <FaWallet /> Connect your wallet
                             </Button>
                         </div>
--- frontend/src/components/home/hero.tsx
+++ frontend/src/components/home/hero.tsx
@@ -7,24 +7,24 @@ const Hero = () => {
         <section className="flex items-center justify-center h-screen">
             <div className="container mx-auto px-4">
                 <div className="flex justify-center mb-5">
-                    <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight shadow-inner">
+                    <div className="text-sm inline-flex border border-[#222]/10 dark:border-[#fff]/10 px-3 py-1 rounded-lg tracking-tight shadow-inner dark:shadow-white/70">
                         Version 1 is here
                     </div>
                 </div>
                 <div className="max-w-[600px] lg:max-w-[900px] mx-auto">
 
-                    <h1 className="text-4xl md:text-5xl lg:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-black/70 text-transparent bg-clip-text text-center">
+                    <h1 className="text-4xl md:text-5xl lg:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-black/70 dark:bg-gradient-to-b dark:from-white dark:to-white/70  text-transparent bg-clip-text text-center">
                         Turn your github contributions into solana rewards.
                     </h1>
 
-                    <p className="md:text-lg tracking-tighter text-black/70 text-center mt-5">
+                    <p className="md:text-lg tracking-tighter text-black/70 dark:text-white/70 text-center mt-5">
                         Contribute your expertise and earn Solana by solving real-world problems.
                     </p>
                 </div>
                 <div className="flex items-center justify-center mt-5">
-                    <Button className="pl-2 py-6 text-sm md:text-base shadow-inner" variant="outline">
+                    <Button className="pl-2 py-6 text-sm md:text-base shadow-inner shadow-white/70" variant="outline">
                         <span className="flex items-center gap-2">
-                            <div className="px-3 py-2 rounded-md shadow-inner shadow-white/70 bg-black text-white">
+                            <div className="px-3 py-2 rounded-md shadow-inner shadow-white/70 dark:shadow-black bg-black dark:bg-white dark:text-black text-white">
                                 <FaGithub />
                             </div>
                             Get started with github
