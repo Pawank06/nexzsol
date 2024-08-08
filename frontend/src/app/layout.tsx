a5ccab53f044b352e29c0d4bf8d5503a41ef76be
PawanK06 <pawan06kumar2003@gmail.com>
2024-08-08 16:22:48 +0530

    remove opengraph

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -5,53 +5,12 @@ import { Metadata } from "next";
 import ogImage from "../app/opengraph-image.png";
 import { ThemeProvider } from "@/components/theme-provider";
 
-export const metadata: Metadata = {
-  metadataBase: new URL("https://www.nexzsol.tech/"),
-  title: "nexzsol",
-  description:
-    "Contribute your expertise and earn Solana by solving real-world problems.",
-  openGraph: {
-    title: "https://www.nexzsol.tech/",
-    description:
-      "Turn your github contributions into solana rewards.",
-    url: "https://www.nexzsol.tech/",
-    siteName: "nexzsol.tech",
-    images: [
-      {
-        url: "https://nexzsol.tech.opengraph-image.png",
-        width: ogImage.width,
-        height: ogImage.height,
-      },
-    ],
-
-    locale: "en-US",
-    type: "website",
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  },
-  twitter: {
-    title: "nexzsol",
-    card: "summary_large_image",
-
-    images: [
-      {
-        url: "https://nexzsol.tech.opengraph-image.png",
-        width: ogImage.width,
-        height: ogImage.height,
-      },
-    ],
-  },
-};
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: 'nexzsol',
+  description: 'Turn your github contributions into solana rewards',
+}
 
 export default function RootLayout({
   children,
