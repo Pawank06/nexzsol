bb416e8cc934695a0b49f583b7aef0540fb7d301
PawanK06 <pawan06kumar2003@gmail.com>
2024-08-14 17:25:41 +0530

    improve state

--- frontend/src/components/home/hero.tsx
+++ frontend/src/components/home/hero.tsx
@@ -1,31 +1,32 @@
 
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { FaGithub } from "react-icons/fa";
 import { Button } from "../ui/button";
 import { useRouter } from "next/navigation";
-import useTokenStore from "@/store"; // Ensure the path is correct
+import { useRoleStore, useTokenStore } from "@/store"; // Ensure the path is correct
 import Link from "next/link";
+import { Loader, Rocket, User } from "lucide-react";
 
 const Hero = () => {
+
+  const [loading, setLoading] = useState(false);
 
   const router = useRouter();
   const setToken = useTokenStore((state) => state.setToken);
   const token = useTokenStore((state) => state.token);
-
-  
+  const { role, setRole } = useRoleStore((state) => ({
+    role: state.role,
+    setRole: state.setRole,
+  }));
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-      // This check ensures the code is only run on the client side
       const queryParams = new URLSearchParams(window.location.search);
-      const retrievedToken = queryParams.get("token")
+      const retrievedToken = queryParams.get("token");
       if (retrievedToken) {
         setToken(retrievedToken);
-        // Remove token from URL
         window.history.replaceState({}, document.title, "/");
-
-        // Redirect to the dashboard page
         setToken(retrievedToken);
         router.push("/select-role");
       }
@@ -33,7 +34,13 @@ const Hero = () => {
   }, [setToken, router]);
 
   const handleGitHubLogin = () => {
+    setLoading(true);
     window.location.href = `${process.env.NEXT_PUBLIC_GITHUB_URL}`;
+  };
+
+  const handleRoleLogin = () => {
+    setLoading(true);
+    router.push(`${role}`);
   };
 
   return (
@@ -55,29 +62,45 @@ const Hero = () => {
           </p>
         </div>
         <div className="flex items-center justify-center mt-5">
-          {
-            token ? <Link href="/select-role">
-              <Button
-            onClick={handleGitHubLogin}
+        {token && !role ? (
+        <Link href="/select-role">
+          <Button
+            className="py-6 text-sm md:text-base shadow-inner shadow-white/70 text-white dark:text-black dark:border-none dark:shadow-black/70 bg-black dark:bg-white gap-1 items-center"
           >
-            <span>
-            Select your role
-            </span>
+            {
+                loading ? <Loader className="animate-spin"/> :  <User className="h-5 w-5" />
+              }
+            <span>Select your role</span>
           </Button>
-              
-            </Link> : <Button
-            variant="outline"
-            className="pl-2 py-6 text-sm md:text-base shadow-inner shadow-white/70 dark:bg-black"
-            onClick={handleGitHubLogin}
-          >
-            <span className="flex items-center gap-2">
-              <div className="px-3 py-2 rounded-md shadow-inner shadow-white/70 dark:shadow-black bg-black dark:bg-white dark:text-black text-white font-medium">
-                <FaGithub />
-              </div>
-              Get started with GitHub
-            </span>
-          </Button>
-          }  
+        </Link>
+      ) : token && role ? (
+        <Button
+          
+          onClick={handleRoleLogin}
+          className="py-6 text-sm md:text-base shadow-inner shadow-white/70 text-white dark:text-black dark:border-none dark:shadow-black/70 bg-black dark:bg-white gap-2"
+        >
+           {
+                loading ? <Loader className="animate-spin"/> :  <Rocket className="h-5 w-5" />
+              }
+          {/* Content for users with both token and role */}
+          <span>Get Started Now</span>
+        </Button>
+      ) : (
+        <Button
+          variant="outline"
+          className="pl-2 py-6 text-sm md:text-base shadow-inner shadow-white/70 dark:bg-black"
+          onClick={handleGitHubLogin}
+        >
+          <span className="flex items-center gap-2">
+            <div className="px-3 py-2 rounded-md shadow-inner shadow-white/70 dark:shadow-black bg-black dark:bg-white dark:text-black text-white font-medium">
+              {
+                loading ? <Loader className="animate-spin"/> :  <FaGithub />
+              }
+            </div>
+            Get started with GitHub
+          </span>
+        </Button>
+      )}
         </div>
       </div>
     </section>
