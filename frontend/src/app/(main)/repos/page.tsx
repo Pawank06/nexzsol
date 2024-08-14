cbba855555de108442f12c0ba7785bd48d9c2901
PawanK06 <pawan06kumar2003@gmail.com>
2024-08-14 17:40:05 +0530

    fix deployment error

--- frontend/src/app/(main)/repos/page.tsx
+++ frontend/src/app/(main)/repos/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import useTokenStore from "@/store";
+import {useTokenStore} from "@/store";
 
 import React, { useState } from "react";
 
