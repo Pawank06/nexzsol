7e22fcfead14e2680c5755a975564922627ff31d
PawanK06 <pawan06kumar2003@gmail.com>
2024-08-14 17:25:59 +0530

    add user roles

--- frontend/src/app/maintainer/page.tsx
+++ frontend/src/app/maintainer/page.tsx
@@ -1,7 +1,19 @@
+'use client'
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useRouter } from "next/navigation";
+import { useRoleStore } from "@/store";
 
 const Dashboard = () => {
+    const router = useRouter();
+  const role = useRoleStore((state) => state.role);
+
+  useEffect(() => {
+    if (role !== 'maintainer') {
+      router.push(`/${role || ''}`);
+    }
+  }, [role, router]);
+  
     return (
         <div>
             <div className="flex flex-col items-center gap-1 text-center">
