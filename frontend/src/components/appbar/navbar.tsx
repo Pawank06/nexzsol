8b5e7cfd1dd7ac661ba864820163502581c69276
PawanK06 <pawan06kumar2003@gmail.com>
2024-08-21 23:14:18 +0530

    add wallet connect in client side

--- frontend/src/components/appbar/navbar.tsx
+++ frontend/src/components/appbar/navbar.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect } from 'react'
 import Logo from '../icons/Logo'
 import Link from 'next/link'
 import { Button } from '../ui/button'
 import {FaWallet} from 'react-icons/fa'
 import { ModeToggle } from '../ModeToggle'
+import { WalletDisconnectButton, WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+import { useWallet } from '@solana/wallet-adapter-react'
 
 const Navbar = () => {
+    const {publicKey, signMessage} = useWallet()
+
+    async function signAndSend(){
+
+        if(!publicKey) return
+        
+        const message = new TextEncoder().encode("sign into nexzsol")
+        const signature = await signMessage?.(message)
+    }
+
+    useEffect(() => {
+        signAndSend()
+    }, [publicKey])
     return (
         <header className="py-4 border-b md:border-none fixed top-0 left-0 right-0 z-10">
             <div className="container mx-auto px-4 ">
@@ -44,10 +61,15 @@ const Navbar = () => {
                             </nav>
                         </div>
                         <div className="flex gap-4 items-center">
-                            <ModeToggle/>
-                            <Button className='flex gap-2 shadow-inner shadow-white/70 dark:shadow-black/70'>
-                            <FaWallet /> Connect your wallet
-                            </Button>
+                            {
+                                publicKey ? (
+
+                                    <WalletDisconnectButton />
+                                ) : (
+                                    
+                                    <WalletMultiButton />
+                                )
+                            }
                         </div>
                 </div>
             </div>
