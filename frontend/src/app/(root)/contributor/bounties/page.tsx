d1f968ec020736624da894d135c7f33dcef8fa78
Krishna Bansal <krishnabansal890@gmail.com>
2024-09-01 15:44:34 +0530

    community bounties added

--- frontend/src/app/(root)/contributor/bounties/page.tsx
+++ frontend/src/app/(root)/contributor/bounties/page.tsx
@@ -1,13 +1,58 @@
-import React from 'react'
+import React, { use } from "react";
+import { useState , useEffect} from "react";
+import { Button } from "@/components/ui/button";
+
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { CoolMode } from "@/components/magicui/cool-mode";
+
+
+
 
 const Bounties = () => {
+  const [bounties, setBounties] = useState([]);
+  useEffect(() => {
+    fetch("https://api.github.com/repos/facebook/react")
+      .then((response) => response.json())
+      .then((data) => setBounties(data));
+  }, []);
   return (
-    <div className='flex justify-center items-center'>
-        <h1 className='text-3xl'>
-            Coming Soon...
-        </h1>
+    <div className="flex justify-center items-center">
+      {bounties.map((item:any) => {
+        return (
+          <Card
+            key={item.repoName}
+            className="shadow-lg hover:shadow-xl transition-shadow duration-300"
+          >
+            <CardHeader>
+              <CardTitle className="text-xl font-semibold">
+                {item.repoName}
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              
+              <CoolMode>
+                <Button
+                  onClick={() => {
+                    window.open(item.repoUrl, "_blank");
+                  }}
+                  className={`w-full bg-blue-500`}
+                   // Disable if repo is already added
+                >
+                  Check out this repo
+                </Button>
+              </CoolMode>
+            </CardContent>
+          </Card>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Bounties
+export default Bounties;
